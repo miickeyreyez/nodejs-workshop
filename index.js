6e4800bdd5264d7a68ae3acda6ff366f52cebc7a
Angel Martinez <miickeyreyez@gmail.com>
2019-07-09 10:24:25 -0500

    Remove comments

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//      extended: true
-//  }));
- // default route
+
  app.get('/', function (req, res) {
      return res.send({ error: true, message: 'hello' })
  });
- // set port
- app.listen(3000, function () {
-     console.log('Node app is running on port 3000');
- });
- module.exports = app;
+
+app.listen(3000, function () {
+  console.log('Node app is running on port 3000');
+});
+
+module.exports = app;
