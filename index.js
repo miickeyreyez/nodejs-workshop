79ee7b63e10dbfb0f14dc46a2db610543256e7d6
Angel Martinez <miickeyreyez@gmail.com>
2019-07-09 10:43:43 -0500

    Adding linter to the project

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
- app.get('/', function (req, res) {
-     return res.send({ error: true, message: 'hello' })
- });
+app.get('/', (req, res) => res.send({ error: true, message: 'hello' }));
 
-app.listen(3000, function () {
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log('Node app is running on port 3000');
 });
 
