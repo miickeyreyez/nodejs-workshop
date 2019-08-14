8bb676c9dee54859f60c6854dea790cea063a81c
Angel Reyes <miickeyreyez@gmail.com>
2019-08-14 10:09:34 -0500

    Create scafolding

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+import log from '../logger';
+import PokemonModule from '../modules';
+import { actions, errors } from '../constants';
+
+class PokemonController {
+}
+
+export { PokemonController };
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
+const { PokemonController } = require('../controllers');
+
+// The router will definde the method, the path and the controller operation
+// router.method('/route', PokemonController.endpointMethod);
 
 export default router;
