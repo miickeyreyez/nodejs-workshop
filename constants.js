24b5901fc4243a6deeb9292e7ddaa89ccffc38ed
Angel Reyes <miickeyreyez@gmail.com>
2019-08-14 09:56:53 -0500

    Update master

---
+++ constants.js
@@ -0,0 +1,28 @@
+const error = {
+};
+
+const actions = {
+};
+
+const ALLOW_ORIGIN = 'Access-Control-Allow-Origin';
+const ALLOW_WILDCARD = '*';
+const ALLOW_HEADERS = 'Access-Control-Allow-Headers';
+const REST_HEADERS_OPTIONS = 'Authorization, '
+    + 'X-API-KEY, Origin, X-Requested-With, '
+    + 'Content-Type, Accept, '
+    + 'Access-Control-Allow-Request-Method';
+const ALLOW_METHODS = 'Access-Control-Allow-Methods';
+const ALLOW = 'Allow';
+const REST_METHODS = 'GET, POST, OPTIONS, PUT, DELETE';
+
+export {
+  error,
+  actions,
+  ALLOW_ORIGIN,
+  ALLOW_WILDCARD,
+  ALLOW_HEADERS,
+  REST_HEADERS_OPTIONS,
+  ALLOW_METHODS,
+  ALLOW,
+  REST_METHODS,
+};
